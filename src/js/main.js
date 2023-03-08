86342366ab793e45d6967d58a6bac49b309a3e67
davidroygsu <124539273+davidroygsu@users.noreply.github.com>
2023-03-08 01:07:58 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+//Import DataTable
+import DataTable from 'datatables.net-dt';
+let table = new DataTable('#tabley', {
+    responsive: true
+});
